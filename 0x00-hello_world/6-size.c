4c6fe8a24327af6c01c887b406edabc185840711
Jany Muong <me.roumuong@gmail.com>
2022-12-08 15:06:06 -0800

    sizeof op

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	char a;
+	char cha;
 	int num;
 	long num2;
-	long long test;
-	float fl;
+	long long numLo;
+	float flo;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(cha));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(num));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(num2));
-	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(test));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(fl));
+	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(numLo));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(flo));
 
 	return (0);
 }
