85e017dd8bc0a721e6f0df7f62cfd9a30f8364eb
Jany Muong <me.roumuong@gmail.com>
2022-12-09 11:18:56 -0800

    Print alpabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Code entry point
+  * Print lowercase alphabet
+  * Return: Return 0 after execution
+  */
+int main(void)
+{
+	char c = 'a';
+	char z = 'z';
+	
+	/* loop through the alphabet */ 
+	for (; c <= z; c++)
+	{
+		putchar(c);
+	}
+
+	return (0);
+}
