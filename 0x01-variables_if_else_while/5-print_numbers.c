92c2d71aa79d2f6a7113dc1982c419726dd7772a
Jany Muong <me.roumuong@gmail.com>
2022-12-09 12:32:05 -0800

    print single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Code entry point
+  * Print unidigits 
+  * Return: Return 0 after execution
+  */
+int main()
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
