e92d6fd14e4bc93610818c10370e2295334d7fea
Jany Muong <me.roumuong@gmail.com>
2022-12-09 15:05:33 -0800

    Final commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 
 /**
   * main - Code entry point
-  * pritn combinat - 2 -digit int < 99 
+  * pritn combinat - 2 -digit int < 99
   * Return: Return 0 after execution
   */
 int main(void)
@@ -14,7 +14,9 @@ int main(void)
 	{
 		for (j = 0; j <= 99; j++)
 		{
-			if (i == j) continue;
+			if (i == j) 
+				continue;
+
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
 			putchar(' ');
