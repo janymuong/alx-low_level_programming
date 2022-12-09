cfb211f159a49cc326c9c34e1c1a38be976ef5fe
Jany Muong <me.roumuong@gmail.com>
2022-12-09 15:00:50 -0800

    Print combination specified

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - Code entry point
+  *
+  * Return: Return 0 after execution
+  */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i == '7' && j == '8' && k == '9')
+						break;
+
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
