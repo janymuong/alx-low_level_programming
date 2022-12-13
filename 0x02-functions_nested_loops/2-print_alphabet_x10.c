e77278a288929428a2acd8528d739fef99660a22
Jany Muong <me.roumuong@gmail.com>
2022-12-13 13:13:16 -0800

    Pritn alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10x
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		char c = 'a', z = 'z';
+
+		while (c <= z)
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
