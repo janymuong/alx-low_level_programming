6013e5d5243c6c7a06417126b39a07e41089d5ea
Jany Muong <me.roumuong@gmail.com>
2022-12-13 14:05:11 -0800

    is_lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - prints the alphabet, if lowercase
- */
+  * _islower - print aphabet if lowercase
+  * @c: character to check
+  *
+  * Return: Returns 1 for lowercase and 0 for uppercase
+  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
