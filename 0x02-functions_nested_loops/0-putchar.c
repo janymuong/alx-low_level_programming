6badca5056a09eb5c3d862a7f5071e94a808d902
Jany Muong <me.roumuong@gmail.com>
2022-12-13 11:54:26 -0800

    Print "_putchar" to stdout

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main
+ * function _putchar()-print char str
+ * '_putchar' w/ while loop
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *strg = "_putchar";
+
+	while (*strg)
+	{
+		_putchar(*strg);
+		strg++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
