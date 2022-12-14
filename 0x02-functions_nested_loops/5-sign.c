623e1bb69e97eb648191e209fff5c7dbf31fb9d7
Jany Muong <me.roumuong@gmail.com>
2022-12-14 05:13:02 -0800

    Pritn sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 /**
-  * print_sign - print sign for int
-  * @n: int to check
+  * print_sign - Print sign of integer
+  * @n: char to check
   *
-  * Return: Returns 1 for positive, 0 for exactly 0, -1 for negative
+  * Return: Returns 1 for n graeter than 0, -1 for less than 0
   */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+
+	else if (n < 0)
 	{
-		_putchar("0");
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
+
 	else
 	{
-		_putchar("-");
-		return (-1);
+		_putchar('0');
+		return (0);
+
 	}
 }
