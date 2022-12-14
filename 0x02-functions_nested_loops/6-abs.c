cf01b523fc2d32b7ee8c8281e7ee71f7dc851bdd
Jany Muong <me.roumuong@gmail.com>
2022-12-14 05:36:42 -0800

    compute the absolute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * abs- print ansolute int
+  * @x: int to check
+  *
+  * Return: Returns value of integer
+  */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-x);
+	}
+	else
+	{
+		return (x);
+	}
+}
