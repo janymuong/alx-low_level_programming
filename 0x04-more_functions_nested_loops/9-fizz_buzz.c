f344cefa8ef731b0b3c0a142f197e9516b4c8829
Jany Muong <me.roumuong@gmail.com>
2022-12-15 06:13:37 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
+void fizz_buzz(void);
+
 /**
  * main - start of program
+ *
+ * prints Fizz for multiples of 3 
+ * pritns Buzz for multiples of 5 
+ * prints FizzBuzz for multiples of 5 & 3
  *
  * Return: Always 0 ()success
  */
 int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
+
+/**
+ * fizz_buzz - print from 1 thro' 100
+ *
+ * Return - void
+ */
+void fizz_buzz(void)
 {
 	int fizz;
 
@@ -29,6 +46,4 @@ int main(void)
 		}
 	}
 	printf("\n");
-
-	return (0);
 }
