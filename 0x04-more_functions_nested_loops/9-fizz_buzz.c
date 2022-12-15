fa3fde0cd3f608326c1e2f7d7c14fcd494a684ff
Jany Muong <me.roumuong@gmail.com>
2022-12-15 05:57:58 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@ void fizz_buzz(void);
 /**
  * main - start of program
  *
- * Return: Always 0.
+ * Return: Always 0 ()success
  */
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
 
 /**
  * fizz_buzz - print from 1 thro' 100
- * 
+ *
  * Return - void
  */
 void fizz_buzz(void)
