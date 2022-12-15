d788b779fe208bb9c4c7402c2120847e2ae43b90
Jany Muong <me.roumuong@gmail.com>
2022-12-15 05:48:10 -0800

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void fizz_buzz(void);
+
+/**
+ * main - start of program
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	/*call Fizz Buzz*/
+	fizz_buzz();
+	return (0);
+}
+
+/**
+ * fizz_buzz - print from 1 thro' 100
+ * 
+ * Return - void
+ */
+void fizz_buzz(void)
+{
+	int fizz;
+
+	for (fizz = 1; fizz <= 100; fizz++)
+	{
+		if (fizz % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (fizz % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (fizz % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", fizz);
+		}
+	}
+	printf("\n");
+}
