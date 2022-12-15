d04ba74efea99ba4a9d08f7c8833f8b38689380b
Jany Muong <me.roumuong@gmail.com>
2022-12-15 06:17:43 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,50 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fizz_buzz(void);
-
 /**
- * main - start of program
- *
- * prints Fizz for multiples of 3 
- * pritns Buzz for multiples of 5 
- * prints FizzBuzz for multiples of 5 & 3
- *
- * Return: Always 0 ()success
+ * main - prints the numbers from 1 to 100
+ * 3 multiples print Fizz instead of the number
+ * 5 multiples print Buzz instead of the number
+ * 3 & 5 multiples print FizzBuzz instead of the number
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fizz_buzz();
-	return (0);
-}
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
-/**
- * fizz_buzz - print from 1 thro' 100
- *
- * Return - void
- */
-void fizz_buzz(void)
-{
-	int fizz;
-
-	for (fizz = 1; fizz <= 100; fizz++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (fizz % 15 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (fizz % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (fizz % 5 == 0)
-		{
-			printf("Buzz ");
-		}
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
 		else
-		{
-			printf("%d ", fizz);
-		}
+			printf("%d ", i);
 	}
 	printf("\n");
+	return (0);
 }
