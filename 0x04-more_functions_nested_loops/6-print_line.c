a211f90f0084d96600eb1a8ec82ecfdf7117437b
Jany Muong <me.roumuong@gmail.com>
2022-12-15 04:32:04 -0800

    Print line in terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print line in terminal
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int cli;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (cli = 0; cli < n; cli++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
