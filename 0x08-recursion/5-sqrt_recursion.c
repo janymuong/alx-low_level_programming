b66c15b4283664bbe2b6cee2965dd40932646154
Jany Muong <me.roumuong@gmail.com>
2023-01-06 00:40:41 +0300

    compute square root recursively

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int _compute_sqrt(int c, int guess);
+
+/**
+  * _compute_sqrt - get square roots and perfect squares by guesses
+  * @guess: random picker of square roots
+  * @c: base number
+  *
+  * Return: returns square root
+  */
+int _compute_sqrt(int c, int guess)
+{
+	if (guess * guess == c)
+	{
+		return (guess);
+	}
+	else if (guess * guess > c)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_compute_sqrt(c, guess + 1));
+	}
+}
+
+/**
+  * _sqrt_recursion - determine square roots
+  * @n: base number
+  *
+  * Return: returns square root
+  */
+int _sqrt_recursion(int n)
+{
+	return (_compute_sqrt(n, 1));
+}
