854dea8e6fbe4774db132a023c262b462e0e4bd6
Jany Muong <me.roumuong@gmail.com>
2023-01-06 00:57:07 +0300

    compare wildcards

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,5 +15,8 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+
 	return (0);
 }
