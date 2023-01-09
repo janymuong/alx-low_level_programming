500d5610c2bc3ab8eb34c025c6e69a35ed8beefa
Jany Muong <me.roumuong@gmail.com>
2023-01-09 22:02:03 +0300

    print sum positive to stdout

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,24 +9,21 @@
  */
 int main(int argc, char **argv)
 {
-	int i, sum_positive = 0;
+	int i, sum = 0;
 
-	if (argc == 1)
-	{
-		printf("%d\n", argc - 1);
+	if (argc < 1)
 		return (0);
-	}
 
 	for (i = 1; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
 		{
-			printf("Error\n");
+			printf("%s\n", "Error");
 			return (1);
 		}
-		sum_positive += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
-	printf("%d\n", sum_positive);
+	printf("%d\n", sum);
 
 	return (0);
 }
