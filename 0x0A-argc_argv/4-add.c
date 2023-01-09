178619a370390aa0caa7fbb9403dd0fc188044bd
Jany Muong <me.roumuong@gmail.com>
2023-01-09 21:26:56 +0300

    print positive sum to stdout

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints sum of positive intgers, followed by \n
+ * @argc: count args passed
+ * @argv: pointer to string array pointing to arguements
+ * Return: exit code 0
+ */
+int main(int argc, char **argv)
+{
+	int i, sum_positive = 0;
+
+	if (argc < 1)
+		return (0);
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum_positive += atoi(argv[i]);
+	}
+	printf("%d\n", sum_positive);
+
+	return (0);
+}
