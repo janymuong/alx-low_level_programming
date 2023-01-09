5afd1bca35639765f533cc85fccf787b8d5d9dd9
Jany Muong <me.roumuong@gmail.com>
2023-01-09 22:26:50 +0300

    change maker

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,47 +9,42 @@
  */
 int main(int argc, char **argv)
 {
-	int coins = 0, cents;
+	int cents, coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	cents = atoi(argv[1]);
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+
 	while (cents > 0)
 	{
-		if (cents >= 25)
+		coins++;
+		if ((cents - 25) >= 0)
 		{
 			cents -= 25;
-			coins++;
+			continue;
 		}
-		else if (cents >= 10)
+		else if ((cents - 10) >= 0)
 		{
 			cents -= 10;
-			coins++;
+			continue;
 		}
-		else if (cents >= 5)
+		else if ((cents - 5) >= 0)
 		{
 			cents -= 5;
-			coins++;
+			continue;
 		}
-		else if (cents >= 2)
+		else if ((cents - 2) >= 0)
 		{
 			cents -= 2;
-			coins++;
+			continue;
 		}
-		else
-		{
-			cents -= 1;
-			coins++;
-		}
+		cents--;
 	}
+
 	printf("%d\n", coins);
 
 	return (0);
