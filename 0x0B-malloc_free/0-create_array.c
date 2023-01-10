bbe499dbd39a78139de8b29729beb0ad7a0ca21f
Jany Muong <me.roumuong@gmail.com>
2023-01-10 23:58:34 +0300

    initialize array with a const char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * *create_array - creates an array of chars, and initializes it with a char.
+  * @size: specifies the size of the array
+  * @c: value to initialize the array with
+  *
+  * Return: pointer to the array if true, NULL/1 if false
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i = 0;
+
+	if (size == 0) 
+		return (NULL);
+	str = (char *)malloc(size * sizeof(char));
+	if (str == NULL)
+	{
+		printf("Error allocating memory!\n");
+		return (NULL);
+	}
+	for (; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
