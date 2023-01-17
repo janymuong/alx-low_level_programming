262b3fc8fc985039627a9c1ad62f09381d42c712
Jany Muong <me.roumuong@gmail.com>
2023-01-17 22:23:44 +0300

    struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+# C - Structures; typedef
+---
+
+Use cases for structures, and syntax for ```struct``` declarations.
+Usage of ```typedef``` in creating alias-like names to reference structs and struct members.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+
+/**
+ * struct dog - UDT for for a typical dog
+ * @name: name the dog
+ * @age: age
+ * @owner: owner person's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void my_dog(struct dog *ptr, char *name, float age, char *owner);
+void print_dog(struct dog *ptr);
+
+/**
+ * dog_t - typedef to alias dog
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *ptr);
+
+#endif /* DOG_H; struct dog */
