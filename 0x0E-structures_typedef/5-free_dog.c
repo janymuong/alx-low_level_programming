b3407e137779bec51a38eea1d93bac60ba9771e7
Jany Muong <me.roumuong@gmail.com>
2023-01-18 01:00:37 +0300

    free() dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free block of memory allocated to struct dog
+ * @d: poijnter to struct dog(dod_t)
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+}
