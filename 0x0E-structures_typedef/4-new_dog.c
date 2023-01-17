9ee6b69109e3d6ca3655665a79b59e0ba19b97db
Jany Muong <me.roumuong@gmail.com>
2023-01-18 00:47:48 +0300

    new dog type struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include "dog.h"
 
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 /**
   * new_dog - creates a new dog type struct dog
   * @name: name thw dog
@@ -9,46 +12,77 @@
   *
   * Return: newly created dog type struct dog
   */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int name_len = 0, owner_len = 0, i = 0;
+	dog_t *new_dog;
+	int name_len, owner_len;
 
-	dog_t *new_dog = NULL;
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
 
-	while (name[name_len] != '\0')
-		name_len++;
-	while (owner[owner_len] != '\0')
-		owner_len++;
-
-	new_dog = malloc(sizeof(struct dog));
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-	{
-		free(new_dog);
 		return (NULL);
-	}
 
-	new_dog->name = malloc(name_len + 1);
+	new_dog->name = malloc(sizeof(char) * (name_len + 1));
 	if (new_dog->name == NULL)
 	{
-		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-
-	new_dog->owner = malloc(owner_len + 1);
+	new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
 		free(new_dog);
+		free(new_dog->name);
 		return (NULL);
 	}
-
-	for (; i <= name_len; i++)
-		new_dog->name[i] = name[i];
-	for (; i <= owner_len; i++)
-		new_dog->owner[i] = owner[i];
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
 	new_dog->age = age;
+
 	return (new_dog);
+}
+
+/**
+ * _strlen - returns the length of string
+ * @s: pointer to string
+ *
+ * Return: the length of the string passed into f(n)
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * *_strcpy - copy string to another
+ * @dest: pointer to destination string
+ * @src: source string pointer
+ *
+ * Return: *pointer dest string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0, i = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
