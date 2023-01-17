17345a9377041d5c29f40b799439de6dff1eb40e
Jany Muong <me.roumuong@gmail.com>
2023-01-17 23:57:36 +0300

    print struct dog to stdout

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		printf("Name: %s\n", d->name);
 
-		if (!(d->age))
+		if (d->age <= 0)
 			printf("Age: (nil)\n");
 		printf("Age: %.6f\n", d->age);
 
