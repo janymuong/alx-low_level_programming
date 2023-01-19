449b10f0e137b36a7b5adb6e603ffa126d3feaaa
Jany Muong <me.roumuong@gmail.com>
2023-01-19 16:58:18 +0300

    Execute a function given as arg

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,8 @@
 #include "function_pointers.h"
 
 /**
-  * array_iterator - executes a function given as a parameter on each element of an array
+  * array_iterator - executes a function 
+  * given as a parameter on each element of an array
   * @size: array size
   * @action: pointer to the function to use
   * @array: integer array to iterate on
