c6e2f1d5e31a9afa29f4fb7c50d23ed4dc8548e5
Jany Muong <me.roumuong@gmail.com>
2023-01-19 17:11:15 +0300

    Execute a function given as arg

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
-  * array_iterator - executes a function 
+  * array_iterator - executes a function
   * given as a parameter on each element of an array
   *
   * @size: array size
@@ -14,13 +13,13 @@
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	size_t i = 0;
 
 	if (size <= 0)
 		return;
 	if (array == NULL || action == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (; i < size; i++)
 		action(array[i]);
 }
