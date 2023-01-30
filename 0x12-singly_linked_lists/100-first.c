5a0cc358782e7d6eb9fa176393b356f3557ed891
Jany Muong <me.roumuong@gmail.com>
2023-01-30 23:04:24 +0300

    print stuff before a main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
 /**
-  * _main_predecessor - print chars before main is executed
+  * _main_pred - print chars before main is executed
   *
-  * Return: void
   */
-void _main_predecessor(void)
+void _main_pred(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
