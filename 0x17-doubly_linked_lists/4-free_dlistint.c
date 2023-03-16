e69b2819e481008f2e2c2f9689cd641cdaf34247
Jany Muong <me.roumuong@gmail.com>
2023-03-16 21:11:26 +0300

    free structure

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	while (head != NULL)
+	{
+		head = head->next;
+		free(head);
+	}
+}
