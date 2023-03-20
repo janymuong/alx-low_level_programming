f50556bed1384f63a29ecf0d61b03ddc2d7361f0
Jany Muong <me.roumuong@gmail.com>
2023-03-20 17:11:35 +0300

    Call C from Python

---
+++ 0x18-dynamic_libraries/c_functions_from_python/c_ops.c
@@ -0,0 +1,59 @@
+/**
+  * add - add two integers
+  * @a: first interger arg
+  * @b: second arg
+  *
+  * Return: c_ops result
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtraction op for two integers
+* @a: first interger arg
+* @b: second arg
+*
+* Return: c_ops result
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiply integers
+* @a: first interger arg
+* @b: second arg
+*
+* Return: c_ops result
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divsion two integers
+* @a: first interger arg
+* @b: second arg
+*
+* Return: c_ops result
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - modulus two integers
+* @a: first interger arg
+* @b: second arg
+*
+* Return: c_ops result
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
