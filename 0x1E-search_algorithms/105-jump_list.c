a0c541e51920580f40265def5a75a85949dd5c27
Jany Muong <me.roumuong@gmail.com>
2023-07-12 16:17:39 +0300

    [fix] jump search in a singly linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -14,34 +14,33 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t jump_step, prev = 0;
+	size_t step, jump_step;
 	listint_t *node, *jump;
 
 	if (list == NULL || size == 0)
 		return (NULL);
 
+	step = 0;
 	jump_step = sqrt(size);
-	jump = list;
-	while (jump->index < size && jump->n < value)
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
 		node = jump;
-		for (prev = 0; jump->next && prev < jump_step; prev++)
-			jump = jump->next;
-		printf("Value checked at index [%lu] = [%d]\n",
+		for (step += jump_step; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
+		printf("Value checked at index [%ld] = [%d]\n",
 				jump->index, jump->n);
 	}
-	printf("Value found between indexes [%lu] and [%lu]\n",
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
 			node->index, jump->index);
 
-	jump = node;
-	while (jump && jump->index < size && jump->n <= value)
-	{
-		printf("Value checked at index [%lu] = [%d]\n",
-				jump->index, jump->n);
-		if (jump->n == value)
-			return (jump);
-		jump = jump->next;
-	}
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n",
+				node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
-	return (NULL);
+	return (node->n == value ? node : NULL);
 }
